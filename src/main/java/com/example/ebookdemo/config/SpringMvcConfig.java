d41ca63fda2213182be010d0b86f3282bceeac13
wanjingyi <e1561846758@163.com>
2024-11-16 15:55:32 +0800

    5-9 增加aop，打印接口耗时，请求参数和返回参数去掉过滤器，拦截器

--- src/main/java/com/example/ebookdemo/config/SpringMvcConfig.java
+++ src/main/java/com/example/ebookdemo/config/SpringMvcConfig.java
@@ -1,49 +1,49 @@
-package com.example.ebookdemo.config;
-
-
-import com.example.ebookdemo.interceptor.LogInterceptor;
-import jakarta.annotation.Resource;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LogInterceptor loginInterceptor;
-
+//package com.example.ebookdemo.config;
+//
+//
+//import com.example.ebookdemo.interceptor.LogInterceptor;
+//import jakarta.annotation.Resource;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
 //    @Resource
-//    ActionInterceptor actionInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns(
-                        "/login",
-                        "/test/**",
-                        "/redis/**",
-                        "/user/login",
-                        "/category/all",
-                        "/ebook/list",
-                        "/doc/all/**",
-                        "/doc/vote/**",
-                        "/doc/find-content/**",
-                        "/ebook-snapshot/**",
-                        "/ebook/upload/avatar",
-                        "/file/**"
-                );
-
-//        registry.addInterceptor(actionInterceptor)
-//                .addPathPatterns(
-//                        "/*/save",
-//                        "/*/delete/**",
-//                        "/*/reset-password");
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/file/**").addResourceLocations("file:D:/file/wiki/");
-    }
-}
+//    LogInterceptor loginInterceptor;
+//
+////    @Resource
+////    ActionInterceptor actionInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(loginInterceptor)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns(
+//                        "/login",
+//                        "/test/**",
+//                        "/redis/**",
+//                        "/user/login",
+//                        "/category/all",
+//                        "/ebook/list",
+//                        "/doc/all/**",
+//                        "/doc/vote/**",
+//                        "/doc/find-content/**",
+//                        "/ebook-snapshot/**",
+//                        "/ebook/upload/avatar",
+//                        "/file/**"
+//                );
+//
+////        registry.addInterceptor(actionInterceptor)
+////                .addPathPatterns(
+////                        "/*/save",
+////                        "/*/delete/**",
+////                        "/*/reset-password");
+//    }
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/file/**").addResourceLocations("file:D:/file/wiki/");
+//    }
+//}
