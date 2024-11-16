9df9253469421c568d0256220fe51ae53f437531
wanjingyi <e1561846758@163.com>
2024-11-16 15:07:55 +0800

    5-5 axios拦截器的添加

--- web/src/main.ts
+++ web/src/main.ts
@@ -21,3 +21,25 @@ for(const i in icons) {
 
 console.log('环境',process.env.NODE_ENV);
 console.log('服务端',process.env.VUE_APP_SERVER);
+
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+    console.log('请求参数',config);
+    return config;
+  }, function (error) {
+    // 对请求错误做些什么
+    console.log('请求错误',error);
+    return Promise.reject(error);
+  });
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+    // 对响应数据做点什么
+    console.log('返回结果',response);
+    return response;
+  }, function (error) {
+    // 对响应错误做点什么
+    console.log('返回错误',error);
+    return Promise.reject(error);
+  });
