f40f7d29e1fea0d29eb9eab93bb66306c66a0ec6
wanjingyi <e1561846758@163.com>
2024-11-16 13:12:22 +0800

    5-2 集成axios

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -49,7 +49,6 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
