dd1aae1c6261b45943ffaf6bb83e7de8b5c0101e
wanjingyi <e1561846758@163.com>
2024-11-16 13:00:52 +0800

    4-7 header,footer分离

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,34 +1,32 @@
 <template>
   <a-layout class="content_ebook">
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu v-model:selectedKeys="selectedKeys1" theme="dark" mode="horizontal" :style="{ lineHeight: '64px' }">
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
+    <the-header></the-header>
 
     <router-view />
 
-    <a-layout-footer style="text-align: center">
-      汪敬一的电子书
-    </a-layout-footer>
+    <the-footer></the-footer>
   </a-layout>
 </template>
 
-<script lang="ts" setup>
-import { ref } from 'vue';
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons-vue';
-const selectedKeys1 = ref<string[]>(['2']);
-const selectedKeys2 = ref<string[]>(['1']);
-const openKeys = ref<string[]>(['sub1']);
+<script lang="ts">
+import { defineComponent } from 'vue'
+import  TheHeader  from '@/components/the-header.vue';
+import  TheFooter  from '@/components/the-footer.vue';
+
+export default defineComponent({
+  name: 'app',
+  components: {
+    TheHeader,
+    TheFooter
+  }
+})
 </script>
 
 <style>
 .content_ebook {
   height: 100%;
 }
+
 #components-layout-demo-top-side-2 .logo {
   float: left;
   width: 120px;
