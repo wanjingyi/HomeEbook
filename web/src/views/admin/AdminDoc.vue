655c8858ad4ef83da07b72f4ac5b492f561879fc
wanjingyi <e1561846758@163.com>
2024-11-27 19:22:23 +0800

    8-12 使用树形组件展示文档树，遗留bug，文档树应该只查当前电子书的文档，而不是全部文档

---
+++ web/src/views/admin/AdminDoc.vue
@@ -0,0 +1,79 @@
+<template>
+    <a-layout>
+        <a-layout-content>
+            <a-row>
+                <a-col :span="6">
+                    <a-tree v-if="level1.length > 0"  :tree-data="level1" :field-names="{ label: 'name', value: 'id', children: 'children' }" :defaultExpandAllRows="true">
+                        <!-- <template #title="{ title, key }">
+                            <span v-if="key === '0-0-1-0'" style="color: #1890ff">{{ title }}</span>
+                            <template v-else>{{ title }}</template>
+                        </template> -->
+                    </a-tree>
+                </a-col>
+                <a-col :span="18">col-6 col-pull-18</a-col>
+            </a-row>
+        </a-layout-content>
+    </a-layout>
+</template>
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue';
+import axios from 'axios';
+import { Tool } from '@/util/tool'
+import { message } from 'ant-design-vue';
+import { useRoute } from 'vue-router';
+
+export default defineComponent({
+    name: 'AdminDoc',
+    setup () {
+        const route = useRoute();
+        const docs = ref();
+        const level1 = ref();
+        level1.value = [];
+
+        /**
+         * 数据查询
+         */
+
+         const handleQuery = () => {
+            axios.get('/document/allData').then((response) => {
+                const data = response.data
+                if (data.success) {
+                    docs.value = data.content
+                    level1.value = [];
+                    level1.value = Tool.array2Tree(docs.value,0);
+                } else {
+                    message.error(data.message);
+                }
+
+            })
+        }
+
+        onMounted(() => {
+            handleQuery();
+        }) 
+
+        return {
+            level1
+        }
+    }
+})
+
+</script>
+<style scoped>
+#components-layout-demo-top-side .logo {
+    float: left;
+    width: 120px;
+    height: 31px;
+    margin: 16px 24px 16px 0;
+    background: rgba(255, 255, 255, 0.3);
+}
+
+.ant-row-rtl #components-layout-demo-top-side .logo {
+    float: right;
+    margin: 16px 0 16px 24px;
+}
+
+.site-layout-background {
+    background: #fff;
+}
+</style>
