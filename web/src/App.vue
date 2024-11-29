a7ca0659b803a09d21082b31c4d7c28fdaba5dde
wanjingyi <e1561846758@163.com>
2024-11-29 13:38:31 +0800

    9-6 解决登陆按钮重复bug

--- web/src/App.vue
+++ web/src/App.vue
@@ -10,21 +10,69 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import  TheHeader  from '@/components/the-header.vue';
-import  TheFooter  from '@/components/the-footer.vue';
+import TheHeader from '@/components/the-header.vue';
+import TheFooter from '@/components/the-footer.vue';
+const ignoreErrors = ["ResizeObserver loop completed with undelivered notifications", "ResizeObserver loop limit exceeded"]
+ 
+window.addEventListener('error', e => {
+  let errorMsg = e.message;
+  ignoreErrors.forEach(m => {
+    if (errorMsg.startsWith(m)) {
+      console.error(errorMsg);
+      if (e.error) {
+        console.error(e.error.stack);
+      }
+      const resizeObserverErrDiv = document.getElementById(
+        'webpack-dev-server-client-overlay-div'
+      );
+      const resizeObserverErr = document.getElementById(
+        'webpack-dev-server-client-overlay'
+      );
+      if (resizeObserverErr) {
+        resizeObserverErr.setAttribute('style', 'display: none');
+      }
+      if (resizeObserverErrDiv) {
+        resizeObserverErrDiv.setAttribute('style', 'display: none');
+      }
+    }
+  })
+});
+
 
 export default defineComponent({
   name: 'app',
   components: {
     TheHeader,
     TheFooter
+  },
+
+  setup() {
+    // const debounce = (fn, delay) => {
+    //   let timer = null;
+    //   return function () {
+    //     let context = this;
+    //     let args = arguments;
+    //     clearTimeout(timer);
+    //     timer = setTimeout(function () {
+    //       fn.apply(context, args);
+    //     }, delay);
+    //   }
+    // }
+
+    // const _ResizeObserver = window.ResizeObserver;
+    // window.ResizeObserver = class ResizeObserver extends _ResizeObserver {
+    //   constructor(callback) {
+    //     callback = debounce(callback, 16);
+    //     super(callback);
+    //   }
+    // }
   }
 })
 </script>
 
 <style>
 .content_ebook {
-  height:auto;
+  height: auto;
 }
 
 #components-layout-demo-top-side-2 .logo {
--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -2,31 +2,40 @@
     <a-layout-header class="header">
         <div class="logo" />
         <a-menu theme="dark" mode="horizontal" :style="{ lineHeight: '64px' }">
-            <a-menu-item key="/">
-                <router-link to="/">首页</router-link>
-            </a-menu-item>
-            <a-menu-item key="/admin/user">
-                <router-link to="/admin/user">用户管理</router-link>
-            </a-menu-item>
-            <a-menu-item key="/admin/ebooks">
-                <router-link to="/admin/ebooks">电子书管理</router-link>
-            </a-menu-item>
-            <a-menu-item key="/admin/category">
-                <router-link to="/admin/category">分类管理</router-link>
-            </a-menu-item>
-            <a-menu-item key="/about">
-                <router-link to="/about">关于我们</router-link>
-            </a-menu-item>
+            <div>
+                <a-menu-item key="/">
+                    <router-link to="/">首页</router-link>
+                </a-menu-item>
+                <a-menu-item key="/admin/user">
+                    <router-link to="/admin/user">用户管理</router-link>
+                </a-menu-item>
+                <a-menu-item key="/admin/ebooks">
+                    <router-link to="/admin/ebooks">电子书管理</router-link>
+                </a-menu-item>
+                <a-menu-item key="/admin/category">
+                    <router-link to="/admin/category">分类管理</router-link>
+                </a-menu-item>
+                <a-menu-item key="/about">
+                    <router-link to="/about">关于我们</router-link>
+                </a-menu-item>
+            </div>
+
             <!-- <a class="login-menu" @click="showLoginModal">
                 登陆
             </a> -->
-            <a-button name="登录"></a-button>
+            <!-- <a-button name="登录"></a-button> -->
+            <div class="unque" @click="showLoginModal">
+                <a class="login-menu" >
+                    <span>登录</span>
+                </a>
+            </div>
+
         </a-menu>
     </a-layout-header>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, onMounted, ref } from 'vue';
 
 export default defineComponent({
     name: 'TheHeader',
@@ -36,6 +45,11 @@ export default defineComponent({
         const showLoginModal = () => {
             loginModalVisible.value = true;
         };
+
+        onMounted (() => {
+            let elements=document.getElementsByClassName("unque") 
+            elements[0].remove(); 
+        });
 
         return {
             showLoginModal,
@@ -48,9 +62,13 @@ export default defineComponent({
 </script>
 
 <style scoped>
-  .login-menu {
-    float: right;
-    color: white;
-    padding-left: 10px;
+.unque{
+    width:700px !important
+  }
+ .unque .login-menu {
+    float: right !important;
+    color: white !important;
+    padding-right: 10px;
+   
   }
 </style>
