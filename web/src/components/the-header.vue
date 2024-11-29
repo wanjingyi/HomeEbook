f7ae2e9e859980a2a9368d612b92c26e3399db7d
wanjingyi <e1561846758@163.com>
2024-11-29 13:50:05 +0800

    9-6 前端增加登录框

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -19,41 +19,80 @@
                     <router-link to="/about">关于我们</router-link>
                 </a-menu-item>
             </div>
-
-            <!-- <a class="login-menu" @click="showLoginModal">
-                登陆
-            </a> -->
-            <!-- <a-button name="登录"></a-button> -->
             <div class="unque" @click="showLoginModal">
-                <a class="login-menu" >
+                <a class="login-menu">
                     <span>登录</span>
                 </a>
             </div>
-
         </a-menu>
+
+        <a-modal title="登录" v-model:visible="loginModalVisible" :confirm-loading="loginModalLoading" @ok="login">
+            <a-form :model="loginUser" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+                <a-form-item label="登录名">
+                    <a-input v-model:value="loginUser.loginName" />
+                </a-form-item>
+                <a-form-item label="密码">
+                    <a-input v-model:value="loginUser.password" type="password" />
+                </a-form-item>
+            </a-form>
+        </a-modal>
     </a-layout-header>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted, ref } from 'vue';
+import axios from 'axios';
+import { message } from 'ant-design-vue';
+declare let hexMd5: any;
+declare let KEY: any;
 
 export default defineComponent({
     name: 'TheHeader',
 
     setup() {
         const loginModalVisible = ref(false);
+        const loginModalLoading = ref(false);
+        // 用来登录
+        const loginUser = ref({
+            loginName: "admin",
+            password: "admin"
+        });
+
         const showLoginModal = () => {
             loginModalVisible.value = true;
         };
 
-        onMounted (() => {
-            let elements=document.getElementsByClassName("unque") 
-            elements[0].remove(); 
+
+        // 登录
+        const login = () => {
+            console.log("开始登录");
+            loginModalLoading.value = true;
+            loginUser.value.password = hexMd5(loginUser.value.password + KEY);
+            axios.post('/user/login', loginUser.value).then((response) => {
+                loginModalLoading.value = false;
+                const data = response.data;
+                if (data.success) {
+                    loginModalVisible.value = false;
+                    message.success("登录成功！");
+
+                    // store.commit("setUser", data.content);
+                } else {
+                    message.error(data.message);
+                }
+            });
+        };
+
+        onMounted(() => {
+            let elements = document.getElementsByClassName("unque")
+            elements[0].remove();
         });
 
         return {
             showLoginModal,
-            loginModalVisible
+            loginModalVisible,
+            loginModalLoading,
+            loginUser,
+            login
 
         }
     }
@@ -62,13 +101,14 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.unque{
-    width:700px !important
-  }
- .unque .login-menu {
+.unque {
+    width: 700px !important
+}
+
+.unque .login-menu {
     float: right !important;
     color: white !important;
     padding-right: 10px;
-   
-  }
+
+}
 </style>
